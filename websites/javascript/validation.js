c121e2d3a85afbf61febbf55fa1ccea42eb1217d
cstone001 <cstone2191@gmail.com>
2019-10-31 17:35:40 -0400

    Remove unnecessary code from validation.js

--- websites/javascript/validation.js
+++ websites/javascript/validation.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-    // Generate a simple captcha
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    function generateCaptcha() {
-        $('#captchaOperation').html([randomNumber(1, 100), '+', randomNumber(1, 200), '='].join(' '));
-    }
-
-    generateCaptcha();
-
     $('#contactForm')
         .formValidation({
             framework: 'bootstrap',
@@ -84,23 +73,7 @@ $(document).ready(function() {
                             message: 'The message must be less than 700 characters long'
                         }
                     }
-                },
-                captcha: {
-                    validators: {
-                        callback: {
-                            message: 'Wrong answer',
-                            callback: function(value, validator, $field) {
-                                var items = $('#captchaOperation').html().split(' '),
-                                    sum   = parseInt(items[0]) + parseInt(items[2]);
-                                return value == sum;
-                            }
-                        }
-                    }
                 }
             }
         })
-        .on('err.form.fv', function(e) {
-            // Regenerate the captcha
-            generateCaptcha();
-        });
 });
